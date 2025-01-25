6c1109443fb259cf188df8dfe9db487d56f5a32c
Anubhav Tripathy <97749731+anubhav241@users.noreply.github.com>
2025-01-25 16:45:52 +0530

    Delete OnlineBookStore directory

--- OnlineBookStore/src/main/java/OnlineBookStore/DBInfo.java
+++
@@ -1,7 +0,0 @@
-package OnlineBookStore;
-
-public interface DBInfo {
-	public static final String  dbUrl = "jdbc:oracle:thin:@localhost:1521:xe";
-	public static final String uName = "c##anubhav";
-	public static final String pWord = "anubhav123";
-}
